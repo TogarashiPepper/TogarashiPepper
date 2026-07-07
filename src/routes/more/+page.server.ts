ab115811a268d27aa4167715e2a0d47f087a7576
TogarashiPepper <git@togarashi.xyz>
2026-07-07 12:37:54 -0400

    more caching

--- src/routes/more/+page.server.ts
+++ src/routes/more/+page.server.ts
@@ -2,7 +2,11 @@ import type { PageServerLoad } from './$types';
 
 export const prerender = false;
 
-export const load: PageServerLoad = async ({ platform }) => {
+export const load: PageServerLoad = async ({ platform, setHeaders }) => {
+	setHeaders({
+		'cache-control': 'public, max-age=3600, s-maxage=3600'
+	});
+
 	let token = platform?.env?.LASTFM_TOKEN;
 	const albumPromise = fetch(`http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=togarashipep&api_key=${token}&limit=1&period=1month&format=json`)
 		.then(r => r.json())
