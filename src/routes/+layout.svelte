8d36bd1a4bd3bfbbb0ff6688b63259c59c6e3b2d
TogarashiPepper <git@togarashi.xyz>
2026-07-19 00:46:34 -0700

    add mastodon to document header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,6 +23,7 @@
 		href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
 		rel="stylesheet"
 	/>
+	<link rel="me" href="https://hachyderm.io/@togarashipepper">
 </svelte:head>
 
 <div class="ml-8 mt-3 flex gap-1">
